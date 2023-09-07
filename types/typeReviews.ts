caae26d5c424c570069b9baf0944b923c3cff144
Kev <sulpeum@gmail.com>
2023-09-07 17:22:02 -0400

    type: add types for anime reviews

---
+++ types/typeReviews.ts
@@ -0,0 +1,60 @@
+export interface IAnimeReviews {
+  pagination: Pagination
+  data: IAnimeReviewData[]
+}
+
+export interface IAnimeReviewData {
+  mal_id: number
+  url: string
+  type: Type
+  reactions: Reactions
+  date: Date
+  review: string
+  score: number
+  tags: Tag[]
+  is_spoiler: boolean
+  is_preliminary: boolean
+  episodes_watched: null
+  user: User
+}
+
+export interface Reactions {
+  overall: number
+  nice: number
+  love_it: number
+  funny: number
+  confusing: number
+  informative: number
+  well_written: number
+  creative: number
+}
+
+export enum Tag {
+  MixedFeelings = "Mixed Feelings",
+  NotRecommended = "Not Recommended",
+  Recommended = "Recommended",
+}
+
+export enum Type {
+  Anime = "anime",
+}
+
+export interface User {
+  url: string
+  username: string
+  images: Images
+}
+
+export interface Images {
+  jpg: Image
+  webp: Image
+}
+
+export interface Image {
+  image_url: string
+}
+
+export interface Pagination {
+  last_visible_page: number
+  has_next_page: boolean
+}
