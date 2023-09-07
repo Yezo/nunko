bff9dead6b35a58f14157dde36735c6636fd1b02
Kev <sulpeum@gmail.com>
2023-09-07 17:30:04 -0400

    feat: add loading page states

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,13 @@
+import { Sidebar } from "@/components/navbar/sidebar/sidebar"
+import { UpdateIcon } from "@radix-ui/react-icons"
+
 export default function Loading() {
-  return <p>Loading Data...</p>
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="grid min-h-screen flex-1 place-items-center">
+        <UpdateIcon className="h-8 w-8 animate-spin text-muted-foreground" />
+      </div>
+    </div>
+  )
 }
