b714f881f56507510c9ba7f645411859d52fff65
Kev <sulpeum@gmail.com>
2023-09-07 17:21:29 -0400

    feat: add rate limiter

--- lib/fetchJikan.ts
+++ lib/fetchJikan.ts
@@ -1,10 +1,11 @@
-import { transformString } from "@/lib/utils"
+import { transformString, wait } from "@/lib/utils"
+import { IAnimeEpisodes } from "@/types/typeEpisodes"
 import { IIndividualAnime } from "@/types/typeIndividualAnime"
 import { IIndividualAnimeCharacters } from "@/types/typeIndividualAnimeChars"
 import { IIndividualAnimeRecommendations } from "@/types/typeIndividualAnimeRecommendations"
 import { ITopAnime } from "@/types/typeTopAnime"
 
-export type getIndividualAnimeProp = {
+export type paramProps = {
   id: string
 }
 
@@ -24,74 +25,70 @@ export type getAllAnimeProp = {
   genres?: string
 }
 
-export async function getAllAnime(searchParams: getAllAnimeProp): Promise<ITopAnime> {
-  const url = transformString(searchParams)
-  const res = await fetch(url)
+//The amount of milliseconds to throttle API calls to prevent rate-limiting
+export const MS = 350
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data")
-  }
+export const handleResponseError = (res: Response) => {
+  if (res.status === 400) throw new Error("400: The developer has made an invalid request.")
+  if (res.status === 404) throw new Error("404: The resource was not found or does not exist.")
+  if (res.status === 429) throw new Error("429: You are currently being rate-limited. ")
+  if (res.status === 500) throw new Error("500: Something went wrong. Please try again.")
+  if (!res.ok) throw new Error("000: Failed to fetch data.")
+}
+
+export async function getAllAnime(param: getAllAnimeProp): Promise<ITopAnime> {
+  const url = transformString(param)
+  const res = await fetch(url)
+  handleResponseError(res)
 
   return res.json()
 }
 
-export async function getIndividualAnime(
-  searchParams: getIndividualAnimeProp
-): Promise<IIndividualAnime> {
-  const url = `https://api.jikan.moe/v4/anime/${searchParams.id}`
+export async function getIndividualAnime(param: paramProps): Promise<IIndividualAnime> {
+  const url = `https://api.jikan.moe/v4/anime/${param.id}`
   const res = await fetch(url)
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data")
-  }
+  await wait(MS)
+  handleResponseError(res)
 
   return res.json()
 }
 
 export async function getIndividualAnimeCharacters(
-  searchParams: getIndividualAnimeProp
+  param: paramProps
 ): Promise<IIndividualAnimeCharacters> {
-  const url = `https://api.jikan.moe/v4/anime/${searchParams.id}/characters`
+  const url = `https://api.jikan.moe/v4/anime/${param.id}/characters`
   const res = await fetch(url)
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data")
-  }
+  await wait(MS)
+  handleResponseError(res)
 
   return res.json()
 }
 
 export async function getIndividualAnimeRecommendations(
-  searchParams: getIndividualAnimeProp
+  param: paramProps
 ): Promise<IIndividualAnimeRecommendations> {
-  const url = `https://api.jikan.moe/v4/anime/${searchParams.id}/recommendations`
+  const url = `https://api.jikan.moe/v4/anime/${param.id}/recommendations`
   const res = await fetch(url)
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data")
-  }
+  await wait(MS)
+  handleResponseError(res)
 
   return res.json()
 }
 
-export async function getIndividualAnimeStaff(searchParams: getIndividualAnimeProp) {
-  const url = `https://api.jikan.moe/v4/anime/${searchParams.id}/staff`
+export async function getIndividualAnimeStaff(param: paramProps) {
+  const url = `https://api.jikan.moe/v4/anime/${param.id}/staff`
   const res = await fetch(url)
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data")
-  }
+  await wait(MS)
+  handleResponseError(res)
 
   return res.json()
 }
 
-export async function getIndividualAnimeEpisodes(searchParams: getIndividualAnimeProp) {
-  const url = `https://api.jikan.moe/v4/anime/${searchParams.id}/episodes`
+export async function getIndividualAnimeEpisodes(param: paramProps): Promise<IAnimeEpisodes> {
+  const url = `https://api.jikan.moe/v4/anime/${param.id}/episodes`
   const res = await fetch(url)
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data")
-  }
+  await wait(MS)
+  handleResponseError(res)
 
   return res.json()
 }
