585e7ba6eaf674cdfb82181d73365e8a3a7e206b
Kev <sulpeum@gmail.com>
2023-09-07 17:30:49 -0400

    feat: add sidebar data

---
+++ components/navbar/sidebar/sidebar-data.tsx
@@ -0,0 +1,63 @@
+import {
+  DesktopIcon,
+  ExclamationTriangleIcon,
+  HomeIcon,
+  ImageIcon,
+  LaptopIcon,
+  PersonIcon,
+  ReaderIcon,
+  TargetIcon,
+} from "@radix-ui/react-icons"
+
+export const SIDEBAR_MAIN_NAV_LINKS = {
+  "/": {
+    name: "home",
+    icon: <HomeIcon />,
+  },
+  "/feed": {
+    name: "feed",
+    icon: <TargetIcon />,
+  },
+}
+
+export const SIDEBAR_EXPLORE_NAV_LINKS = {
+  "/search/anime": {
+    name: "anime",
+    icon: <DesktopIcon />,
+  },
+  "/manga": {
+    name: "manga",
+    icon: <ImageIcon />,
+  },
+  "/movies": {
+    name: "movies",
+    icon: <ReaderIcon />,
+  },
+  "/anime/1": {
+    name: "cowboy bebop",
+    icon: <ReaderIcon />,
+  },
+  "/anime/21": {
+    name: "one piece",
+    icon: <ReaderIcon />,
+  },
+  "/anime/10": {
+    name: "error page",
+    icon: <ExclamationTriangleIcon />,
+  },
+}
+
+export const SIDEBAR_LIBRARY_NAV_LINKS = {
+  "/DSADSA": {
+    name: "profile",
+    icon: <PersonIcon />,
+  },
+  "/manga": {
+    name: "my anime",
+    icon: <LaptopIcon />,
+  },
+  "/movies": {
+    name: "my manga",
+    icon: <ReaderIcon />,
+  },
+}
--- components/navbar/sidebar/sidebar.tsx
+++ components/navbar/sidebar/sidebar.tsx
@@ -1,40 +1,114 @@
-import { ThemeToggle } from "@/components/themes/theme-toggle"
+"use client"
+import {
+  SIDEBAR_EXPLORE_NAV_LINKS,
+  SIDEBAR_LIBRARY_NAV_LINKS,
+  SIDEBAR_MAIN_NAV_LINKS,
+} from "@/components/navbar/sidebar/sidebar-data"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import {
+  CrumpledPaperIcon,
+  DesktopIcon,
+  DotsVerticalIcon,
+  ExclamationTriangleIcon,
+  GearIcon,
+  HomeIcon,
+  ImageIcon,
+  LaptopIcon,
+  MagnifyingGlassIcon,
+  PersonIcon,
+  ReaderIcon,
+  TargetIcon,
+} from "@radix-ui/react-icons"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
-type Props = {}
-export const Sidebar = ({}: Props) => {
+export const Sidebar = () => {
+  let pathname = usePathname() || "/"
+  if (pathname.includes("/anime/")) pathname = "/anime"
+  if (pathname.includes("/search/anime/top-100")) pathname = "/anime"
+
   return (
     <aside className="sticky top-0 hidden min-h-screen w-56  self-start border-r lg:grid">
-      <div className="flex flex-col gap-4">
-        <Link href="/" className="mb-20 font-domine text-xl font-semibold tracking-tight">
-          Nunko.
-        </Link>
+      <div className="flex flex-col justify-between px-6 py-8">
+        <div>
+          <Link
+            href="/"
+            className="flex items-center gap-2 font-domine text-xl font-semibold tracking-tight"
+          >
+            <div className="rounded-full border border-muted/80 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-600 via-gray-800 to-black p-1.5 text-foreground shadow-sm">
+              <CrumpledPaperIcon />
+            </div>
+            Nunko.
+          </Link>
 
-        <p className="font-medium">Explore</p>
-        <Link href="/" className="text-muted-foreground">
-          Home
-        </Link>
-        <Link href="/" className="text-muted-foreground">
-          Feed
-        </Link>
+          <div className="flex items-center py-4 pt-8">
+            <Input
+              placeholder="Search..."
+              type="text"
+              className="cursor-pointer placeholder:text-xs"
+            />
+            <MagnifyingGlassIcon className="-m-8 cursor-pointer text-muted-foreground" />
+          </div>
 
-        <p className="font-medium">Explore</p>
-        <Link href="/" className="text-muted-foreground">
-          Anime
-        </Link>
-        <Link href="/" className="text-muted-foreground">
-          Manga
-        </Link>
-        <Link href="/anime/21" className="text-muted-foreground">
-          One Piece
-        </Link>
-        <Link href="/anime/54538" className="text-muted-foreground">
-          Zombies
-        </Link>
+          <nav className="flex flex-col gap-2">
+            <p className="font-medium"></p>
+            {Object.entries(SIDEBAR_MAIN_NAV_LINKS).map(([path, { name, icon }]) => {
+              return <LinkItem url={path} key={path} name={name} icon={icon} />
+            })}
 
-        <Link href="/search/anime/top-100">Top 100</Link>
-        <ThemeToggle />
+            <Separator className="my-2" />
+
+            <p className="font-medium tracking-tight">Explore</p>
+            {Object.entries(SIDEBAR_EXPLORE_NAV_LINKS).map(([path, { name, icon }]) => {
+              return <LinkItem url={path} key={path} name={name} icon={icon} />
+            })}
+
+            <Separator className="my-2" />
+
+            <p className="font-medium">Library</p>
+            {Object.entries(SIDEBAR_LIBRARY_NAV_LINKS).map(([path, { name, icon }]) => {
+              return <LinkItem url={path} key={path} name={name} icon={icon} />
+            })}
+          </nav>
+        </div>
+
+        <div className="flex items-center justify-between text-sm">
+          <div className="flex items-center gap-3">
+            <Avatar className="h-8 w-8">
+              <AvatarImage src="https://avatars.githubusercontent.com/u/6843026?v=4" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div>Munkie</div>
+          </div>
+
+          <Button size="icon" variant="ghost">
+            <DotsVerticalIcon className="h-[1rem] w-[1rem]" />
+          </Button>
+        </div>
       </div>
     </aside>
+  )
+}
+
+const LinkIcon = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="rounded border  border-muted/80 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-gray-900 to-gray-800 p-1.5 shadow-sm">
+      {children}
+    </div>
+  )
+}
+
+const LinkItem = ({ url, icon, name }: { url: string; icon: JSX.Element; name: string }) => {
+  return (
+    <Link
+      href={url}
+      className="group/list-item hover:text-slate-0 flex h-9 w-full flex-row items-center justify-start gap-2 truncate rounded-md text-sm  capitalize text-slate-300 transition-colors hover:text-white"
+    >
+      <LinkIcon>{icon}</LinkIcon>
+      {name}
+    </Link>
   )
 }
