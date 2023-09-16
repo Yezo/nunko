e456dbc4637369b84332a100177f2e24a3a02b32
Kev <sulpeum@gmail.com>
2023-09-16 13:48:52 -0400

    fix: made sure both same functionality

--- components/navbar/sidebar/sidebar-search-input.tsx
+++ components/navbar/sidebar/sidebar-search-input.tsx
@@ -2,18 +2,15 @@
 
 import { Input } from "@/components/ui/input"
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons"
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
 import { useDebouncedCallback } from "use-debounce"
 
 export const SidebarSearchInput = () => {
-  const BASE_URL = usePathname() // Get the current pathname = '/search/anime/top-100'
-  const BASE_TYPE = "q" // Lowercase base = 'order_by', 'sort', 'type'
-  const searchParams = useSearchParams() // Get the query parameters from the URL
-  const queryParam = searchParams.get("q")
-
-  const [value, setValue] = useState(queryParam)
   const router = useRouter()
+  const searchParams = useSearchParams()
+  const queryParam = searchParams.get("q")
+  const [value, setValue] = useState(queryParam)
 
   const debounced = useDebouncedCallback((value) => {
     setValue(value)
@@ -23,18 +20,18 @@ export const SidebarSearchInput = () => {
     if (value) {
       router.push(`/search?q=${value}`)
     }
-  }, [BASE_URL, router, searchParams, value])
+  }, [router, value])
 
   return (
     <div className="mb-4 mt-8 flex items-center">
       <Input
         placeholder="Search..."
         type="text"
-        className="cursor-pointer text-xs capitalize placeholder:text-xs placeholder:capitalize"
+        className="text-xs capitalize placeholder:text-xs placeholder:capitalize"
         defaultValue={""}
         onChange={(e) => debounced(e.target.value)}
       />
-      <MagnifyingGlassIcon className="-m-8 cursor-pointer text-muted-foreground" />
+      <MagnifyingGlassIcon className="-m-8 text-muted-foreground" />
     </div>
   )
 }
--- components/search/search-input.tsx
+++ components/search/search-input.tsx
@@ -1,50 +1,37 @@
 "use client"
 
 import { Input } from "@/components/ui/input"
-import { MagnifyingGlassIcon, UpdateIcon } from "@radix-ui/react-icons"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons"
 import { useEffect, useState } from "react"
-import { useDebounce, useDebouncedCallback } from "use-debounce"
-import { getSearchAnime } from "@/lib/fetchJikan"
-import { ITopAnime, ITopAnimeData } from "@/types/anime/type-top-anime"
-import { useRouter } from "next/navigation"
+import { useDebouncedCallback } from "use-debounce"
+import { useRouter, useSearchParams } from "next/navigation"
 
-type Props = {}
-export const SearchInput = ({}: Props) => {
-  const [value, setValue] = useState("")
-  const [data, setData] = useState<ITopAnimeData[] | null>(null) // Initialize data state to store API response
-  const [loading, setLoading] = useState(false) // Add a loading state
-  const [db] = useDebounce(value, 1000)
+export const SearchInput = () => {
   const router = useRouter()
+  const searchParams = useSearchParams()
+  const queryParam = searchParams.get("q")
+  const [value, setValue] = useState(queryParam)
 
   const debounced = useDebouncedCallback((value) => {
     setValue(value)
-  }, 1000)
+  }, 1500)
 
   useEffect(() => {
-    // Call fetchData when debouncedValue changes (user stops typing)
-    if (db && db.length > 0) {
-      router.push(`/search?q=${db}`)
+    if (value) {
+      router.push(`/search?q=${value}`)
     }
-  }, [db, router])
+  }, [router, value])
 
   return (
     <div className="flex items-center py-4 pt-8">
       <Input
         placeholder="Search..."
         type="text"
-        className="cursor-pointer text-xs placeholder:text-xs"
+        className="text-xs capitalize placeholder:text-xs placeholder:capitalize"
         defaultValue={""}
         onChange={(e) => debounced(e.target.value)}
       />
-      <MagnifyingGlassIcon className="-m-8 cursor-pointer text-muted-foreground" />
+      <MagnifyingGlassIcon className="-m-8 text-muted-foreground" />
     </div>
   )
 }
