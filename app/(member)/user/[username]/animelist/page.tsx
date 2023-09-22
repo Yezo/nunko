400fc42b45d730136e5618eaa102dbf08944c063
Kev <sulpeum@gmail.com>
2023-09-22 16:56:09 -0400

    feat: add no result state

--- app/(member)/user/[username]/animelist/page.tsx
+++ app/(member)/user/[username]/animelist/page.tsx
@@ -97,6 +97,48 @@ export default async function UsernameAnimeListPage({
           )
         return null
       })}
+
+      {/* Check if no sections were rendered and display the desired section */}
+      {sections.every((section) => {
+        const hasData = section.data.length > 0
+        const listQueryMatches = list?.toLowerCase() === section.title?.toLowerCase()
+        return !(hasData && (listQueryMatches || !list))
+      }) && (
+        <section className="mt-8 grid min-h-[625px] place-items-center rounded border">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <BirdSVG />
+            <div className="text-center">
+              <div className="text-lg font-medium tracking-tight text-foreground">No results!</div>
+              <div className="text-center text-sm text-muted-foreground">
+                There may be too many filters applied.
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
     </>
+  )
+}
+
+const BirdSVG = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="42"
+      height="42"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M16 7h.01" />
+      <path d="M3.4 18H12a8 8 0 0 0 8-8V7a4 4 0 0 0-7.28-2.3L2 20" />
+      <path d="m20 7 2 .5-2 .5" />
+      <path d="M10 18v3" />
+      <path d="M14 17.75V21" />
+      <path d="M7 18a6 6 0 0 0 3.84-10.61" />
+    </svg>
   )
 }
