a4b9055922884b03639fac605e3487309c029f7b
Kev <sulpeum@gmail.com>
2024-07-01 00:26:37 -0400

    fix: unoptimize manga cards

--- components/manga/top/manga-card.tsx
+++ components/manga/top/manga-card.tsx
@@ -20,6 +20,7 @@ export const MangaCard = ({ data }: MangaCardProps) => {
           <div className="space-y-2">
             <div className="relative h-[240px] sm:h-[295px] ">
               <Image
+                unoptimized
                 src={data.images.webp.large_image_url}
                 alt={data.mal_id.toString()}
                 fill
